fd1ba6841c5210efe555f489c394b5106535096a
Alexander Boone <alboone@uw.edu>
2020-08-17 21:49:23 -0700

    imported and implemented ReactCardFlip component for use with each ExerciseCard

--- src/components/Content.js
+++ src/components/Content.js
@@ -12,7 +12,7 @@ export default function Content(props) {
     
     return (
         <Grid container spacing={2} xs={11} md={8} justify="center">
-            <ExerciseCard numSets="5" numReps="5" exerciseName="Back Squat" />
+            <ExerciseCard numSets="6" numReps="15, 12, 10, 8, 8, 6" exerciseName="Back Squat" exerciseDescription= "The barbell back squat is a popular compound movement that emphasizes building the lower-body muscle groups and overall strength. It's the classic way to start a leg day, and is a worthy centerpiece to a lower-body training program." />
             <ExerciseCard numSets="5" numReps="5" exerciseName="Back Squat" />
             <ExerciseCard numSets="5" numReps="5" exerciseName="Back Squat" />
             <ExerciseCard numSets="5" numReps="5" exerciseName="Back Squat" />
--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, Typography, makeStyles } from '@material-ui/core'
 
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+
+import ReactCardFlip from 'react-card-flip';
 
 
 const useStyles = makeStyles({
@@ -13,10 +15,12 @@ const useStyles = makeStyles({
   },
   flexContent: {
     display: 'flex',
+
   },
   actions: {
       display: 'flex',
       justifyContent: 'center',
+      flexWrap: 'wrap',
   },
   title: {
   },
@@ -36,28 +40,51 @@ const useStyles = makeStyles({
 
 export default function ExerciseCard(props) {
     const classes = useStyles();
+    const [isFlipped, handleFlip] = useState(false)
+
+    function handleClick() {
+        handleFlip(!isFlipped)
+    }
 
     return (
         <Grid container justify="center" item spacing={1}>
             <Grid item xs={12}>
-                <Card className={classes.root} raised="true">
-                    <CardContent className={classes.flexContent}>
-                        <CardContent>
-                            <Typography className={classes.countText} noWrap="true" variant="h4" component="h2" color="primary">
-                                {props.numSets} X {props.numReps}
-                            </Typography>
+                <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+                    <Card className={classes.root} raised="true">
+                        <CardContent className={classes.flexContent}>
+                            <CardContent>
+                                <Typography className={classes.countText} variant="h4" component="h2" color="primary">
+                                    <strong>{props.exerciseName}</strong>
+                                </Typography>
+                            </CardContent>
+                            <CardContent>
+                                <Typography className={classes.title} variant="h4" component="h2" color="primary">
+                                    {props.numSets} X {props.numReps}
+                                </Typography>
+                            </CardContent>
                         </CardContent>
-                        <CardContent>
-                            <Typography className={classes.title} variant="h4" component="h2" color="primary">
-                                {props.exerciseName}
-                            </Typography>
+                        <CardActions className={classes.actions}>
+                            <Button onClick={handleClick} color="primary" size="medium">Exercise Definition</Button>
+                        </CardActions>
+                    </Card>
+                    <Card className={classes.root} raised="true">
+                        <CardContent className={classes.flexContent}>
+                            <CardContent>
+                                <Typography className={classes.countText} noWrap="true" variant="h4" component="h2" color="primary">
+                                    IMG
+                                </Typography>
+                            </CardContent>
+                            <CardContent>
+                                <Typography className={classes.title} variant="p" component="p" color="primary">
+                                    {props.exerciseDescription}
+                                </Typography>
+                            </CardContent>
                         </CardContent>
-                    </CardContent>
-                    <CardActions className={classes.actions}>
-                        <Button color="primary" size="medium">Definition</Button>
-                        <Button color="primary" size="medium">Muscles Targeted</Button>
-                    </CardActions>
-                </Card>
+                        <CardActions className={classes.actions}>
+                            <Button onClick={handleClick} color="primary" size="medium">Exercise Name</Button>
+                        </CardActions>
+                    </Card>
+                </ReactCardFlip>
             </Grid>
         </Grid>
     )
