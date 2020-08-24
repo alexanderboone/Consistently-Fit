a7071acf5130ff5769986ffd906713aff2c0f535
Alexander Boone <alboone@uw.edu>
2020-08-23 22:09:58 -0700

    updated header to include links (broken for now) in toolbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
 import SvgFitnessCenterIcon from '@material-ui/icons/FitnessCenter';
 
 const useStyles = makeStyles({
+    toolbarStyles: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
     iconStyles: {
         padding: '0% 0% 0% 0%'
     },
     typographyStyles: {
-        flex: 1
+    },
+    navBarStyles: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        width: "100%",
+        padding: "0 0 0 1em",
+    },
+    navButtonStyles: {
+        
     }
 })
 
@@ -18,10 +30,26 @@ export default function Header(props) {
     
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography className={classes.typographyStyles} variant="h4" color="secondary">
-                WOTD
-                </Typography>
+            <Toolbar className={classes.toolbarStyles}>
+                <Button>
+                    <a href="/">
+                        <Typography className={classes.typographyStyles} variant="h4" color="secondary">
+                        AB
+                        </Typography>
+                    </a>
+                </Button>
+                <div className={classes.navBarStyles}>
+                    <Button className={classes.navButtonStyles} color="secondary" size="small">
+                        <Typography className={classes.typographyStyles} color="secondary" variant="body1">
+                        <strong>Workouts</strong>
+                        </Typography>
+                    </Button>
+                    <Button className={classes.navButtonStyles} color="secondary" size="small">
+                        <Typography className={classes.typographyStyles} color="secondary" variant="body1">
+                        About
+                        </Typography>
+                    </Button>
+                </div>
                 <a href="/"><SvgFitnessCenterIcon className={classes.iconStyles}/></a>
             </Toolbar>
         </AppBar>
